6b1c0b47c25a0e316ce7b0160e9043927a420a9b
Stepan <stepanzuev20051@gmail.com>
2025-11-27 12:30:53 +0300

    some fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,6 @@ public class Main {
 
         } catch (Exception ex) {
             System.out.println("Error processing file '" + filename + "': " + ex.getMessage());
-            ex.printStackTrace();
         }
     }
 
